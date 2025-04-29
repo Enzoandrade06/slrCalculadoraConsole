ca3a0bd2b4cd0ca39b7272a6ab9e721c90632454
LabInfo <LabInfo@TERREOA2M14>
2025-04-28 22:18:01 -0300

    Adicionar arquivos de projeto.

---
+++ prjCalculadoraConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace prjCalculadoraConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Somar(10, 10);   
+            Console.WriteLine("10 + 10 é igual a: ");
+            Console.WriteLine(result);
+        }
+        static double Somar(double x, double y)
+        {
+            
+            return x + y;
+        }
+    }
+}
